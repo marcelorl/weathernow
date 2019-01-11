95ff475d63cffee82e6c835892e794b2c15881e8
marcelorl <marcelo.lima@grupopan.com>
2019-01-11 13:50:28 -0200

    Updates

---
+++ precache-manifest.7aba6d9a6879aaae2e53d026626c9118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45377fd606e81a3caefe9f57e624931f",
+    "url": "/weathernow/static/media/logo.45377fd6.svg"
+  },
+  {
+    "revision": "d542c686fe5742a713e3f0038a3a7a8e",
+    "url": "/weathernow/static/media/loader.d542c686.svg"
+  },
+  {
+    "revision": "8a56a087256b80099eb9",
+    "url": "/weathernow/static/js/runtime~main.8a56a087.js"
+  },
+  {
+    "revision": "702a570d79d1def0d0e6",
+    "url": "/weathernow/static/js/main.702a570d.chunk.js"
+  },
+  {
+    "revision": "95b92d1ea5decd311f0f",
+    "url": "/weathernow/static/js/1.95b92d1e.chunk.js"
+  },
+  {
+    "revision": "702a570d79d1def0d0e6",
+    "url": "/weathernow/static/css/main.97b9090f.chunk.css"
+  },
+  {
+    "revision": "11dea0638386234578b026988bc697da",
+    "url": "/weathernow/index.html"
+  }
+];
